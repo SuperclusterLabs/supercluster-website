e2070859568451dfc6fae0ca58448725a2f045dd
Kai Huang <kai@supercluster.dev>
2022-09-12 16:27:19 -0700

    fix: input and button properties

--- components/email_form.tsx
+++ components/email_form.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default class EmailForm extends React.Component {
   constructor(props: any) {
@@ -9,11 +9,27 @@ export default class EmailForm extends React.Component {
     return (
       <>
         <p className="text-4xl">Subscribe for updates</p>
-        <br /><br />
-        <form className="w-full max-w-sm" action="https://getform.io/f/fdb80384-b894-46d2-acd6-7dc455b8555d" method="POST" accept-charset="UTF-8">
+        <br />
+        <br />
+        <form
+          className="w-full max-w-sm"
+          action="https://getform.io/f/fdb80384-b894-46d2-acd6-7dc455b8555d"
+          method="POST"
+          acceptCharset="UTF-8"
+        >
           <div className="flex items-center border-b border-teal-500 py-2">
-            <input className="block p-4 text-2xl appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" name="email" required="required" placeholder="Email" aria-label="email" />
-            <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-2xl border-4 text-white py-1 px-2 rounded" type="submit">
+            <input
+              className="block p-4 text-2xl appearance-none bg-transparent border-none w-full text-white-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="email"
+              name="email"
+              required
+              placeholder="Email"
+              aria-label="email"
+            />
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-2xl border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
               Submit
             </button>
           </div>
